d236405b6a0f6abf0f939b315ba90a86f766c1a3
Camille Regnault <c.regnault.pro@gmail.com>
2019-06-03 18:17:49 +0200

    change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  resources :roleModels, only: [:index, :show]
+  resources :role_models, only: [:index, :show]
   resources :challenges, only: [:index, :show]
   resources :participations
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
