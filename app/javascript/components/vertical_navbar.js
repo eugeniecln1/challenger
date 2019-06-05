6cfd4edc8b256f1a56b478cc74e9d7bf4c6f4de0
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-06-05 18:04:37 +0200

    logo web changed and navbar

--- app/javascript/components/vertical_navbar.js
+++ app/javascript/components/vertical_navbar.js
@@ -1,13 +1,13 @@
 // import $ from "jquery";
 
 // const initVerticalNavbar = () => {
-//   $(function(){
-//      $('#collapse').click(function(){
-//         console.log("hello")
-//         $('#menu').toggle() // AFFICHE ET CACHE A CHAQUE CLIQUE SUR LE BOUTTON
-//      });
-//   });
-// }
+//    $(document).ready(function () {
 
-// export default initVerticalNavbar
+//     $('.nav-linkCollapse').on('click', function () {
+//         $('.nav-link').toggleClass('active');
+//     });
+
+// });
+
+// export { initVerticalNavbar }
 
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,6 @@ deployDescription();
 
 import { initStarRating } from '../plugins/init_star_rating';
 initStarRating();
+
+// import { initVerticalNavbar } from "../components/vertical_navbar"
+// initVerticalNavbar();
