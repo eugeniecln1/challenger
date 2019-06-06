d3262a521ad8449835dacf220fd604ac506753b4
antoine leprince <leprince.antoine7@gmail.com>
2019-06-06 16:25:06 +0200

    js changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,18 +1,17 @@
 import "bootstrap";
 
 
+import { initStarRating } from "../plugins/init_star_rating";
+initStarRating();
+
 import { buttonBack } from "../components/button_back";
 buttonBack();
 
 import { deployDescription } from "../components/card_category";
 deployDescription();
 
-import { initStarRating } from "../plugins/init_star_rating";
-initStarRating();
-
 import initPolar from "../components/dashboard";
-
-initPolar()
+initPolar();
 
 
 // import { initVerticalNavbar } from "../components/vertical_navbar"
