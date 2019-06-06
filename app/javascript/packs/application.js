cf4f677cfdcf39b951cf3d55d574a42f9054873e
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-06-06 12:55:24 +0200

    fixed bugs with javascript

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,13 +1,16 @@
 import "bootstrap";
 
-import initPolar from "../components/dashboard";
-initPolar()
+import { buttonBack } from "../components/button_back";
+buttonBack();
 
 import { deployDescription } from "../components/card_category";
 deployDescription();
 
-import { initStarRating } from '../plugins/init_star_rating';
+import { initStarRating } from "../plugins/init_star_rating";
 initStarRating();
+
+import initPolar from "../components/dashboard";
+initPolar();
 
 // import { initVerticalNavbar } from "../components/vertical_navbar"
 // initVerticalNavbar();
