6cc10242c162165d87cdc200de0b9d583761168b
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-06-06 12:58:54 +0200

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,10 +1,10 @@
 import "bootstrap";
 
-import initPolar from "../components/dashboard";
-initPolar()
-
 import { deployDescription } from "../components/card_category";
 deployDescription();
 
 import { initStarRating } from '../plugins/init_star_rating';
 initStarRating();
+
+import initPolar from "../components/dashboard";
+initPolar()
