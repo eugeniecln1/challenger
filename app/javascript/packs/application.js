8e79458eae37dea5d972b74ddf9b5189a844f051
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-06-06 13:00:21 +0200

    test

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,11 +1,8 @@
 import "bootstrap";
 
-<<<<<<< HEAD
-=======
 import { buttonBack } from "../components/button_back";
 buttonBack();
 
->>>>>>> 00152a1aa266391439ba0e3a6c8d8ed53181f580
 import { deployDescription } from "../components/card_category";
 deployDescription();
 
@@ -13,11 +10,8 @@ import { initStarRating } from "../plugins/init_star_rating";
 initStarRating();
 
 import initPolar from "../components/dashboard";
-<<<<<<< HEAD
 initPolar()
-=======
-initPolar();
+
 
 // import { initVerticalNavbar } from "../components/vertical_navbar"
 // initVerticalNavbar();
->>>>>>> 00152a1aa266391439ba0e3a6c8d8ed53181f580
