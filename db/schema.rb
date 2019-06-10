0be5e86ced42a02504aa71c75d06edd212bce7d9
Sandra Vilder <sandra.vilder@gmail.com>
2019-06-10 16:20:23 +0200

    hon comme il est trop mignooooon ce dragoooon ! -> add a picture to our role model mascotte !

--- db/schema.rb
+++ db/schema.rb
@@ -55,7 +55,6 @@ ActiveRecord::Schema.define(version: 2019_06_10_100807) do
     t.string "picture"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "status"
   end
 
   create_table "users", force: :cascade do |t|
