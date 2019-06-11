4b7d62c2df2db2a6aa7d1e263a5136ed8cd8c0c9
Sandra Vilder <sandra.vilder@gmail.com>
2019-06-11 19:46:36 +0200

    des illus et des challenge épurés

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -7,13 +7,13 @@ class Challenge < ApplicationRecord
 
   def show_category_logo
     if category == "Daily activities"
-      @logo = "illustrations/bermuda-done.png"
+      @logo = "illustrations/flamenco-uploading-1.png"
     elsif category == "Future me"
-      @logo = "illustrations/bermuda-success.png"
+      @logo = "illustrations/flamenco-page-under-construction.png"
     elsif category == "Interactions"
-      @logo = "illustrations/bermuda-logged-out.png"
+      @logo = "illustrations/flamenco-welcome.png"
     elsif category == "Curiosity"
-      @logo = "illustrations/bermuda-searching.png"
+      @logo = "illustrations/flamenco-logged-out.png"
     end
   end
 
