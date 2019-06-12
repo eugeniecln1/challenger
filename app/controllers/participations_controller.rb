b703113cccf7ae80d07ac9293e5a70063290d6e5
antoine leprince <leprince.antoine7@gmail.com>
2019-06-12 17:40:03 +0200

    color chart + btn challenge me + journal card order by updated_at

--- app/controllers/participations_controller.rb
+++ app/controllers/participations_controller.rb
@@ -1,6 +1,6 @@
 class ParticipationsController < ApplicationController
   def index
-    @participations = current_user.participations
+    @participations = current_user.participations.order(updated_at: :desc)
     @bookmarkeds = current_user.bookmarkeds
   end
 
