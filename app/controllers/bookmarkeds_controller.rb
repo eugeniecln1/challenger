18208dd65d53096ad2f738d9059d21771650df83
Eugenie Colonna d Istria <eugeniecolonnadistria@gmail.com>
2019-06-12 18:03:15 +0200

    rolemodel#view modified with ajax

--- app/controllers/bookmarkeds_controller.rb
+++ app/controllers/bookmarkeds_controller.rb
@@ -5,17 +5,28 @@ class BookmarkedsController < ApplicationController
     @bookmarked.role_model = @role_model
     @bookmarked.user = current_user
     if @bookmarked.save
+      respond_to do |format|
+        format.html { redirect_to role_model_path(@role_model) }
+        format.js
+      end
       flash[:notice] = "You have successfully bookmarked this role model 🥳 ! Check out all your bookmarked items in your journal"
     else
+      respond_to do |format|
+        format.html { render 'role_models/show' }
+        format.js
+      end
       flash[:alert] = "Oops! 😱 a problem has occurred"
     end
-    redirect_to role_model_path(@role_model)
   end
 
   def destroy
     @bookmarked = Bookmarked.find(params[:id])
+    @role_model = @bookmarked.role_model
     @bookmarked.destroy
+    respond_to do |format|
+      format.html { redirect_to request.referer }
+      format.js
+    end
     flash[:notice] = "This role model has been unbookmarked"
-    redirect_to request.referer
   end
 end
