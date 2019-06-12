1f02784c1e693d4fb66e9e73fef704718cd9d8f3
Camille Regnault <c.regnault.pro@gmail.com>
2019-06-12 11:59:48 +0200

    navbar transition on pages

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,16 +1,18 @@
 import $ from "jquery";
 import "bootstrap";
 import initPolar from "../components/dashboard";
-import { toggleNav } from "../components/vertical_navbar"
+import { toggleNav } from "../components/vertical_navbar";
 import { initStarRating } from "../components/init_star_rating";
 import { animateChallengeButton } from "../components/button_challenge";
-import initConfetti from "../components/init_confetti.js"
-import { autocompleteSearch } from "../components/autocomplete"
+import initConfetti from "../components/init_confetti.js";
+import { autocompleteSearch } from "../components/autocomplete";
+import { initUpdateNavbarOnScroll } from "../components/navbar";
 
 initPolar();
 toggleNav();
 initStarRating();
 initConfetti();
+initUpdateNavbarOnScroll();
 
 if (document.getElementById('search-data')) {
   autocompleteSearch();
